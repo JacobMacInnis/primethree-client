bbd7192d7b93d05516cbdb748358ca830ffb4eb1
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-12-27 15:17:31 -0500

    http post request working and adding to mongodb

---
+++ src/components/helpers/helpers.js
@@ -0,0 +1,5 @@
+export const inputs = [{text: '1st Digit', name: 'string1'}, {text: '2nd Digit', name: 'string2'}, {text: '3rd Digit', name: 'string3'}];
+
+export const acceptableInputs = ['', ' ','0','1','2','3','4','5','6','7','8','9'];
+
+export const numbers = ['0','1','2','3','4','5','6','7','8','9'];
--- src/components/prime-input.js
+++ src/components/prime-input.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import Input from './input';
+import { inputs, acceptableInputs, numbers } from './helpers/helpers';
+import axios from 'axios';
+import { API_BASE_URL } from '../config';
 
 class PrimeInput extends Component {
   constructor(props) {
@@ -8,18 +11,18 @@ class PrimeInput extends Component {
       string1: '',
       string2: '',
       string3: '',
-      errorMessage: null
+      errorMessage: null,
+      httpErr: null
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   handleChange(event) {
-    let numbers = ['0','1','2','3','4','5','6','7','8','9'];
     let string = event.target.value;
-  
     let char = string.substring(string.length -1);
-    if (numbers.indexOf(char) === -1) {
+    if (acceptableInputs.indexOf(char) === -1) {
       this.setState({
-        errorMessage: 'Error: Must be a single digit number (0,1,2,3,4,5,6,7,8,9)'
+        errorMessage: 'Error: Must be single digit number ex. (0,1,2,3,4,5,6,7,8,9)'
        })
     } else {
       this.setState({
@@ -28,18 +31,38 @@ class PrimeInput extends Component {
       });
     }
   }
+  handleSubmit(event) {
+    event.preventDefault();
+    if (numbers.indexOf(this.state.string1) === -1 || numbers.indexOf(this.state.string2) === -1 || numbers.indexOf(this.state.string3) === -1) {
+      this.setState({
+        errorMessage: 'Error: all inputs must contain numbers'
+      });
+    } else {
+      let primeInput = `${this.state.string1}${this.state.string2}${this.state.string3}`;
+      this.setState({ loading: true });
+      axios.post(`${API_BASE_URL}/primes`, {input: primeInput})
+      .then(res => {
+        console.log(res);
+        console.log('after')
+        console.log(res.data);
+      })
+      .catch(error => {
+        this.setState({ httpErr: error.response });
+      })
+    }
+  }
   render() {
-    const inputs = [{text: '1st Digit', name: 'string1'}, {text: '2nd Digit', name: 'string2'}, {text: '3rd Digit', name: 'string3'}];
-
     let numberInputs = inputs.map((input, i) => {
       return <Input key={i} text={input.text} name={input.name} id={input.name} current={this.state[input.name]} onChange={this.handleChange}/>  
     });
 
     return (
-      <div className="prime-input">
+      <div className="prime-input" onSubmit={this.handleSubmit}>
         <form>
-          {this.state.errorMessage ? <div>{this.state.errorMessage}</div> : ''}
+          <div>{this.state.errorMessage ?           this.state.errorMessage : ''}
+          </div>
           {numberInputs}
+          <input type='submit' value='Submit' />
         </form>
       </div>
     );
